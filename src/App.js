1374351f80a91f9e1243a416217d7ac8500a2575
Silvia Feng <silvia.feng@mediushealth.org>
2020-02-16 17:04:51 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+const API = 'https://picsum.photos/200/';
+
+async function getCards() {
+  const responses = await Promise.all([...Array(8)].map(() => fetch(`${API}`)));
+  return responses.map(response => response.url);
+}
+
+function shuffleCards(array) {
+  return array.sort(() => {
+    return 0.5 - Math.random()
+  })
+}
+
+const CARD_STATE = {
+  UNREVEALED: 'UNREVEALED',
+  REVEALED: 'REVEALED',
+  MATCHED: 'MATCHED',
+};
+
+const Cards = () => {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const data = await getCards()
+      let shuffledCards = shuffleCards([...data, ...data])
+      setCards(shuffledCards.map((card, idx) => {
+        return {
+          position: idx,
+          url: card,
+          state: CARD_STATE.UNREVEALED
+        }
+      }))
+    })();
+  }, [])
+
+  return (
+    <div className="container">
+        {cards.map((item, idx) => <div className="image" key={idx}></div>)}
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cards />
     </div>
   );
 }
